3697b8e97cb0299bbad0253ffb10902e69d8f0aa
jenniferkvpham <79018310+jenniferkvpham@users.noreply.github.com>
2023-09-16 15:56:06 +0100

    debug: resolving new page render bug

--- coach-connect-test/src/App.js
+++ coach-connect-test/src/App.js
@@ -1,104 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './components/views/header';
-import TopSection from './components/views/topSection';
-import ScrollToSection from './components/views/scrollToSection';
+import Home from './components/views/home';
 import About from './components/views/about';
 import Faqs from './components/views/faqs';
-import SignUpPage from './components/routes/SignUpPage';
+import SignUp from './components/views/signUp';
 
 function App() {
-  const [currentTargetPage, setCurrentTargetPage] = useState('about-section');
-  const [hasScrolledToFaqs, setHasScrolledToFaqs] = useState(false);
-
-
-    const updateTargetPage = () => {
-      // Determine the next target page based on the current target page
-      const nextTargetPage = currentTargetPage === 'about-section' ? 'faq-section' : 'about-section';
-
-      // Update the target page
-      setCurrentTargetPage(nextTargetPage);
-    };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const faqSection = document.getElementById('faq-section');
-      if (faqSection) {
-        const scrollTop = window.scrollY || document.documentElement.scrollTop;
-        const faqSectionTop = faqSection.getBoundingClientRect().top + scrollTop;
-
-        // Determine the scroll threshold when you want to hide the ScrollToSection
-        const scrollThreshold = faqSectionTop - window.innerHeight / 2;
-
-        setHasScrolledToFaqs(scrollTop >= scrollThreshold);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [setHasScrolledToFaqs]);
-
-
-  const faqData = [
-    {
-      question: "What is Coach Connect?",
-      answer: "Coach Connect is a platform that uses a machine learning to match employees with development coaches to help them reach their professional goals, overcome challenges, and maximize wellness. "
-    },
-    {
-      question: "How do I get started?",
-      answer: "To sign up as a coach, go to our registration page and fill out the necessary information."
-    },
-    {
-      question: "How does the matching algorithm work?",
-      answer: "The algorithm is..."
-    },
-    {
-      question: "Why Development Coaching?",
-      answer: "Development coaching..."
-    },
-    {
-      question: "What Development Areas are available?",
-      answer: "Areas available include..."
-    },
-    // Add more FAQ items as needed
-  ];
 
   return (
     <Router>
       <div>
-        <Header />
-        <TopSection id='top-section' />
-          <div id='about-section'>
-            <About />
-          </div>
-        <div id='faq-section'>
-          <Faqs faqData={faqData} />
-          <div className="center-button">
-            <button className="get-started">
-              <Link to="/signup">Get Started!</Link>
-            </button>
-          </div>
-          </div>
-
-
+        {/* Define your routes */}
         <Routes>
+          {/* Define a route for the root URL ('/') */}
+          <Route path="/" element={<Home />} /> {/* Replace 'Home' with your main component */}
           <Route path="/about" element={<About />} />
-          <Route path="/faqs" element={<Faqs faqData={faqData} />} />
-          <Route path="/signup" element={<SignUpPage />} /> {/* Sign-up page route */}
+          <Route path="/faqs" element={<Faqs />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* Add more routes as needed */}
         </Routes>
-
-        <ScrollToSection
-          text={'Find Out More'}
-          targetPage={currentTargetPage}
-          updateTargetPage={updateTargetPage}
-          hideScroll={hasScrolledToFaqs}
-        />
       </div>
     </Router>
   );
 }
+
 
 export default App;
